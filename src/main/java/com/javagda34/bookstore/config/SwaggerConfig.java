36038938bf7145f47a2759344a3e8d138a69a23c
Paweł Recław <p.reclaw@apsystems.tech>
2020-08-08 14:32:51 +0200

    Swagger added to project

---
+++ src/main/java/com/javagda34/bookstore/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.javagda34.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.javagda34.bookstore"))
+                .paths(regex("/.*"))                                        // wszystkie endpointy
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("SDA Rest Test API")
+                .description("SDA Rest Test API")
+                .version("1.0.0")
+                .contact(new Contact("Paweł Recław", "http://sdacademy.pl/", "pawel.reclaw@gmail.com"))
+                .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/documentation/**").addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/documentation/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/documentation/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/documentation/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/documentation/swagger-resources", "/swagger-resources");
+    }
+}
--- src/main/java/com/javagda34/bookstore/controller/AuthorController.java
+++ src/main/java/com/javagda34/bookstore/controller/AuthorController.java
@@ -3,6 +3,10 @@ package com.javagda34.bookstore.controller;
 import com.javagda34.bookstore.model.dto.AuthorInfo;
 import com.javagda34.bookstore.model.dto.CreateAuthorDto;
 import com.javagda34.bookstore.service.AuthorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "/author", description = "Operations about authors")
 @RestController // rest controller zwraca obiekty (które sam serializuje do json)
                 // controller - (w odróżnieniu) zwraca nazwy/adresy widoków do request dispachera - który je ładuje
 @RequestMapping(path = "/author")
@@ -17,6 +22,12 @@ import java.util.List;
 public class AuthorController {
     private final AuthorService authorService;
 
+    @ApiOperation(value = "Use to add Author to database.",
+            notes = "Author with identical first and last name can't exist in database.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "Properly added author to database."),
+            @ApiResponse(code = 400, message = "Author with same name, last name and birth date is already in database.")
+    })
     @PostMapping("")
     public ResponseEntity<Void> submitAuthor(@RequestBody CreateAuthorDto author){
         boolean success = authorService.createAuthor(author);
